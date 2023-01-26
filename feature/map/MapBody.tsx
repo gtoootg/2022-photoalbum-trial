8cfdbc7c67e1e75b26be364003c97befb1ab75d1
michihiro.goto <michihiro.goto@techpilot.net>
2023-01-26 14:29:19 +0100

    commit

--- feature/map/MapBody.tsx
+++ feature/map/MapBody.tsx
@@ -1,12 +1,40 @@
 import GoogleMapApi from "../../components/google-map/GoogleMapApi";
+import { useContext } from "react";
+import {
+  categoriesContext,
+  flickrImagesContext,
+  uploadedPostsContext,
+} from "../../pages/_app";
+import {
+  useGetCategories,
+  useGetFlickrImages,
+  useGetUploadedPosts,
+} from "../home/HomeBody";
 
 export const MapBody = () => {
+  const [flickrImages, setFlickrImages] = useContext(flickrImagesContext);
+  const [uploadedPosts, setUploadedPosts] = useContext(uploadedPostsContext);
+
+  useGetUploadedPosts(setUploadedPosts, uploadedPosts);
+  useGetFlickrImages(setFlickrImages, flickrImages);
+
+  const getLocationOfUploadedPosts =
+    uploadedPosts &&
+    uploadedPosts.map((post) => {
+      return {
+        lat: post.lat,
+        lng: post.lng,
+      };
+    });
+
   return (
     <div style={{ height: "90vh", width: "100%" }}>
       <GoogleMapApi
         center={{ lat: 0, lng: 0 }}
         zoom={3}
-        clusterLocations={[]}
+        clusterLocations={
+          getLocationOfUploadedPosts ? getLocationOfUploadedPosts : []
+        }
       />
     </div>
   );
