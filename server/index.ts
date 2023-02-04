1c78a738d7f4d143666f0e362a484114ea6755c4
michihiro.goto <michihiro.goto@techpilot.net>
2023-02-04 16:24:01 +0100

    fix deploy issue

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,21 @@
 import express, { Request, Response } from "express";
 import next from "next";
 
-const dev = process.env.NODE_ENV === "development";
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const port = process.env.PORT;
 
 app.prepare().then(() => {
   const server = express();
+  const port = process.env.PORT;
 
   server.all("*", (req: Request, res: Response) => {
     return handle(req, res);
   });
+
   server.listen(port, (err?: any) => {
     if (err) throw err;
-    console.log(`> Ready on port:${port} - env ${process.env.NODE_ENV}`);
+    console.log(port)
+    console.log(`> Ready on port:${process.env.PORT} - env ${process.env.NODE_ENV}`);
   });
 });
