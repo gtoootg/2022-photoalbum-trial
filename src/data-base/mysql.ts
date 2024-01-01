826488c8ae01730531a29cd423968bb7aa0ebefd
michihiro.goto <michihiro.goto@techpilot.net>
2024-01-01 16:35:08 +0100

    removed nonneeded files

--- src/data-base/mysql.ts
+++
@@ -1,9 +0,0 @@
-
-
-export enum PhotoAlbumTable {
-  PHOTOALBUM_POST = "heroku_5c84548920294f1.`photoalbum-post`",
-  PHOTOALBUM_FLICKR_IMAGE = "heroku_5c84548920294f1.`photoalbum-flickr-image`",
-  PHOTOALBUM_CATEGORY = "heroku_5c84548920294f1.`photoalbum-category`",
-
-  COMMON_CATEGORY = "heroku_5c84548920294f1.`common-category`",
-}
