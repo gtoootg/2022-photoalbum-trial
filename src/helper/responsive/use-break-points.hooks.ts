81669af833f1bf82b55828872b22150e52ad96d6
michihiro.goto <michihiro.goto@techpilot.net>
2024-02-13 08:11:38 +0100

    added todo

--- src/helper/responsive/use-break-points.hooks.ts
+++ src/helper/responsive/use-break-points.hooks.ts
@@ -8,6 +8,7 @@ export const useBreakPoints = () => {
   const isDownSm = useMediaQuery(theme.breakpoints.down("sm"));
   const isDownMd = useMediaQuery(theme.breakpoints.down("md"));
   const isDownLg = useMediaQuery(theme.breakpoints.down("lg"));
+  const isDownXl = useMediaQuery(theme.breakpoints.down("xl"));
 
   const isUpSm = useMediaQuery(theme.breakpoints.up("sm"));
   const isUpMd = useMediaQuery(theme.breakpoints.up("md"));
@@ -21,6 +22,27 @@ export const useBreakPoints = () => {
       isUpSm,
       isUpMd,
       isUpLg,
+      isDownXl,
     };
-  }, [theme, isDownSm, isDownMd, isDownLg, isUpSm, isUpMd, isUpLg]);
+  }, [theme, isDownSm, isDownMd, isDownLg, isUpSm, isUpMd, isUpLg, isDownXl]);
+};
+
+export const useCurrentWidth = () => {
+  const { isDownSm, isDownMd, isDownLg, isDownXl } = useBreakPoints();
+
+  return useMemo(() => {
+    if (isDownSm) {
+      return "xs";
+    }
+    if (isDownMd) {
+      return "sm";
+    }
+    if (isDownLg) {
+      return "md";
+    }
+    if (isDownXl) {
+      return "lg";
+    }
+    return "xl";
+  }, [isDownSm, isDownMd, isDownLg, isDownXl]);
 };
