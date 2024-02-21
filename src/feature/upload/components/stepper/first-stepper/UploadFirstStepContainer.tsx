aa0d7ef026d78cb077cd98423c8db572a4124925
michihiro.goto <michihiro.goto@techpilot.net>
2024-02-21 15:54:47 +0100

    temp

--- src/feature/upload/components/stepper/first-stepper/UploadFirstStepContainer.tsx
+++ src/feature/upload/components/stepper/first-stepper/UploadFirstStepContainer.tsx
@@ -1,6 +1,7 @@
 import ImageListBox from "../../image-list-box/ImageListBox";
 import { PreviewImageListBox } from "../../../../../components/preview-image-list-box/PreviewImageListBox";
 import { useUploadActiveStep } from "../../../state/use-upload-data.reactive-vars";
+import React from "react";
 
 export default function UploadFirstStepContainer() {
   const [activeStep] = useUploadActiveStep();
