68cdc8d01d58e63611fc100b4aae5ad381c87654
michihiro.goto <michihiro.goto@techpilot.net>
2024-02-21 15:42:27 +0100

    added todo

--- src/feature/album-post/components/general-information/components/text-box/AlbumPostGeneralInformationTextGroup.test.tsx
+++ src/feature/album-post/components/general-information/components/text-box/AlbumPostGeneralInformationTextGroup.test.tsx
@@ -2,22 +2,39 @@ import React from "react";
 import { AlbumPostGeneralInformationTextGroup } from "./AlbumPostGeneralInformationTextGroup";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { MgButton } from "../../../../../../components/button/MgButton";
+import { useGetAlbumPostData } from "../../../../hooks/use-get-album-post.hooks";
 
 const mockAlbumPostData = {
+  id: 0,
   title: "Mock Title",
   description: "Mock Description",
+  country: "111",
+  lat: 0,
+  lng: 0,
+  imageIds: [0],
+  categoryIds: {},
+  userId: 0,
 };
-jest.mock("../../../../hooks/use-get-album-post.hooks", () => ({
-  useGetAlbumPostData: () => ({ albumPost: mockAlbumPostData }),
-}));
+
+jest.mock("../../../../hooks/use-get-album-post.hooks");
+const mockUseGetAlbumPostData = jest.mocked(useGetAlbumPostData);
 
 describe("AlbumPostGeneralInformationTextGroup", () => {
-  it("", () => {
+  beforeEach(() => {
+    mockUseGetAlbumPostData.mockReset();
+  });
+
+  it("should show correct title and description", () => {
+    mockUseGetAlbumPostData.mockReturnValue({
+      albumPost: mockAlbumPostData,
+      imageSrcs: [],
+    });
     const { getByText } = render(<AlbumPostGeneralInformationTextGroup />);
 
     const text = getByText("Mock Title");
+    const description = getByText("Mock Description");
 
     expect(text).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
   });
 });
